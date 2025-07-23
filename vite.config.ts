e4440dbccea8f0e6660bc3876b038bcc10e34c7f
Gauri Sharma <gaurisharma1686@gmail.com>
2025-07-22 22:10:25 -0400

    fixed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
-    allowedHosts: true,
     host: "0.0.0.0",
     port: 5173,
   },
@@ -17,4 +16,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
